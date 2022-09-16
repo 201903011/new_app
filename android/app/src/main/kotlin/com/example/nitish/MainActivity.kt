801f9cb453e98f5b1077498d727e893722d0755d
201903011 <72931833+201903011@users.noreply.github.com>
2022-09-16 17:52:59 +0530

    done

---
+++ android/app/src/main/kotlin/com/example/nitish/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nitish
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/category.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+
+class Category extends ChangeNotifier {
+  final String name;
+  final String imageUrl;
+
+  Category({
+    required this.name,
+    required this.imageUrl,
+  });
+}
